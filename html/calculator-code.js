6de601cccd480a6865928dd38de2a4e315b06cca
ataramaruma <102532550+ataramaruma@users.noreply.github.com>
2022-04-26 09:30:06 +0300

    Add files via upload

---
+++ html/calculator-code.js
@@ -0,0 +1,24 @@
+let currentResult = 0;
+
+function add(num1, num2) {
+  const resultAdd = num1 + num2;
+  return resultAdd;
+}
+
+function divide(num1, num2) {
+  const resultDivide = num1 / num2;
+  return resultDivide;
+}
+
+function Subtraction(num1,num2) {
+  const resultSubtraction = num1 - num2;
+  return resultSubtraction;
+}
+
+function duplicate(num1,num2) {
+    const resultduplicate = num1 - num2;
+    return resultduplicate;
+}
+
+addingResult = add(1,3);
+dividingResult = divide(1,4);
